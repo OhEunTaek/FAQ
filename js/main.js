656b4bd3b93f60092537371f366538b6a51779a8
OhEunTaek <giloet777@gmail.com>
2022-04-27 20:29:27 +0900

    FAQ

---
+++ js/main.js
@@ -0,0 +1,43 @@
+﻿const dl = document.querySelector("dl");
+const dts = dl.querySelectorAll("dt");
+const dds = dl.querySelectorAll("dd");
+
+
+dl.addEventListener("click",(e)=>{
+
+    if(e.target == dl) return;
+    let targetdt = e.target.closest("dt");
+    
+    for(let el of dds){
+        el.classList.remove("on")
+        }
+    for(let el of dts){
+        el.classList.remove("on");
+    }
+    targetdt.classList.add("on");
+    targetdt.nextElementSibling.classList.add("on");
+    
+
+    
+
+
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
